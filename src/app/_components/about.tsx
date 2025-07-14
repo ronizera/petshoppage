034a6db12fbdb8fa642c1e7918aece8fb0962dd1
ronizera <roniltonbreno@gmail.com>
2025-07-13 22:11:55 -0300

    FIX BUGS

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import about1Img from "../../../public/about-1.png";
 import about2Img from "../../../public/about-2.png";
+import { Check, MapPin } from "lucide-react";
+import { WhatsappLogoIcon } from "@phosphor-icons/react/dist/ssr";
 
 export function About() {
   return (
     <section className="bg-[#FDF6ec] py-16">
-      <div className="container mx-auto grid-cols-1 lg:grid-cols-2 gap-12 items-center">
-        <div className="relative">
+      <div className="container px-4 mx-auto">
+        <div className=" grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+                 <div className="relative">
           <div className="relative w-full h-[400px] rounded-3xl overflow-hidden">
             <Image
               src={about1Img}
@@ -27,6 +30,46 @@ export function About() {
               priority
             />
           </div>
+        </div>
+
+        <div className='space-y-6 mt-10'>
+          <h2 className='text-4xl font-bold'>SOBRE</h2>
+
+          <p>
+            Until one has loved an animal, a part of one's soul remains unawakened. We believe in it and we believe in easy acess to things that are good for our mind, body and spirit. With a clever offering, superb support and a secure checkout you're in good hands.
+          </p>
+
+          <ul className="space-y-4">
+            <li className="flex items-center gap-2">
+              <Check className="text-red-500"/>
+              Aberto desde 2006.
+            </li>
+
+            <li className="flex items-center gap-2">
+              <Check className="text-red-500"/>
+              Equipe com mais de 10 veterinários.
+            </li>
+
+            <li className="flex items-center gap-2">
+              <Check className="text-red-500"/>
+              Qualidade é nossa priopridade.
+            </li>
+          </ul>
+
+          <div className="flex gap-2">
+            <a href="#"
+            className="bg-[#E84C3D] text-white flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md">
+              <WhatsappLogoIcon className="w-5 h-5 text-white" />
+              Contato via Whatsapp
+            </a>
+            <a href="#"
+            className="flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md">
+              <MapPin className="w-5 h-5 text-black" />
+              Endereço da loja
+            </a>
+          </div>
+        </div>
+
         </div>
       </div>
     </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { Hero } from "./_components/hero";
 import { About } from "./_components/about";
+import { Services } from "./_components/services";
+import { Testimonials } from "./_components/testimonials";
+import { Footer } from "./_components/footer";
 
 export default function Home() {
   return(
     <main>
       <Hero />
       <About />
+      <Services />
+      <Testimonials />
+      <Footer />
     </main>
   )
 }
