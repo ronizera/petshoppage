281942f2aa029f9f0593cf23f68eabb2ba76e4a4
ronizera <roniltonbreno@gmail.com>
2025-07-13 22:15:12 -0300

    fix bugs

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -36,7 +36,7 @@ export function About() {
           <h2 className='text-4xl font-bold'>SOBRE</h2>
 
           <p>
-            Until one has loved an animal, a part of one's soul remains unawakened. We believe in it and we believe in easy acess to things that are good for our mind, body and spirit. With a clever offering, superb support and a secure checkout you're in good hands.
+           Até que alguém ame um animal, uma parte da sua alma permanece adormecida. Acreditamos nisso e acreditamos no fácil acesso a coisas que são boas para a nossa mente, corpo e espírito. Com uma oferta inteligente, suporte excelente e um checkout seguro, você está em boas mãos.
           </p>
 
           <ul className="space-y-4">
